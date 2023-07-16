36cad48c02c8638d0a4f051cc9a16a47808fc6c3
tomas1018 <tomasfernando61@gmail.com>
2023-07-16 15:08:34 -0300

    primera parte del script generar

---
+++ scripts/generar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cantidad=$1
+wget -O "../nombres.txt" "https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv"
+mkdir -p "../imagenes"
+nombres=($(cat "../nombres.txt"))
+
+for ((i=0; i<$cantidad; i++)); do
+  nombre=${nombres[$RANDOM % ${#nombres[@]}]}
+  wget "https://thispersondoesnotexist.com/" -O "../imagenes/$nombre.jpg"
+  sleep 2
+done
+
+exit 0
+
+
