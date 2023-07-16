45f92a30ac69b2fc33b467084daf0f88dc021406
tomas1018 <tomasfernando61@gmail.com>
2023-07-16 16:28:58 -0300

    generar.sh avanzado

--- scripts/generar.sh
+++ scripts/generar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cantidad=$1
-wget -O "../nombres.txt" "https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv"
-mkdir -p "../imagenes"
+wget -O "../nombres.txt" "https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv" mkdir -p "../imagenes"
 nombres=($(cat "../nombres.txt"))
 
 for ((i=0; i<$cantidad; i++)); do
@@ -9,6 +8,10 @@ for ((i=0; i<$cantidad; i++)); do
   wget "https://thispersondoesnotexist.com/" -O "../imagenes/$nombre.jpg"
   sleep 2
 done
+
+tar zcvf imagenes.tar.gz "../imagenes"
+
+md5sum imagenes.tar.gz > "../verificacion.txt"
 
 exit 0
 
