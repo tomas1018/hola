5ee63d70c440ed5ab9a7d2b4c2e4ca9b5d9d7c1a
tomas1018 <tomasfernando61@gmail.com>
2023-07-20 19:20:43 -0300

    finalizacion de los scripts sin comentar

--- scripts/comprimir.sh
+++ scripts/comprimir.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
-nombres=$(ls "./imagenes")
+if [ ! -d ../imagenes ] || [ -z ../imagenes ]; then 
+   echo "Todavia no generaste imagenes o la carpeta se encuentra vacia"
 
-if [ -e nom_img.txt ] ;then
-  rm nom_img.txt
-  touch nom_img.txt
+elif [ ! -d ./imagenes ] || [ -z ./imagenes ] ; then
+   echo "Todavia no Descomprimiste los archivos o la carpeta se encuentra vacia"
+
+elif [ ! -d ../imagenes_reducidas ] ; then
+   echo "Todavia no procesaste las imagenes"
+
 else
-  touch nom_img.txt
+   NOMBRES=$(ls "./imagenes")
+   if [ -e nom_img.txt ] ;then
+     rm nom_img.txt
+     touch nom_img.txt
+   else
+     touch nom_img.txt
+   fi
+
+   for IMG in $NOMBRES; do
+     NOMBRE_ARCH=$(basename "$IMG")
+     echo "${NOMBRE_ARCH%.*}" >> nom_img.txt
+   done
+
+   if [ -e nom_val.txt ] ;then
+     rm nom_val.txt
+     touch nom_val.txt
+   else
+     touch nom_val.txt
+   fi
+
+   egrep  ^[A-Z][a-z]+,?[0-9]* nom_img.txt >> nom_val.txt
+
+   if [ -e nom_a.txt ] ;then
+     rm nom_a.txt
+     touch nom_a.txt
+   else
+     touch nom_a.txt
+   fi
+
+   egrep  a,?[0-9]*$ nom_img.txt >> nom_a.txt
+
+   tar zcvf resulado_final.tar.gz "./imagenes" "./nom_img.txt" "./nom_val.txt" "./nom_a.txt" "../imagenes_reducidas"
+   echo "los archivos fueron procesados"
 fi
-
-for img in $nombres; do
-   nombre_arch=$(basename "$img")
-   echo "${nombre_arch%.*}" >> nom_img.txt
-done
-
-if [ -e nom_val.txt ] ;then
-  rm nom_val.txt
-  touch nom_val.txt
-else
-  touch nom_val.txt
-fi
-
-egrep  ^[A-Z][a-z]+,?[0-9]* nom_img.txt >> nom_val.txt
-
-if [ -e nom_a.txt ] ;then
-  rm nom_a.txt
-  touch nom_a.txt
-else
-  touch nom_a.txt
-fi
-
-egrep  a,?[0-9]*$ nom_img.txt >> nom_a.txt
-
-tar zcvf resulado_final.tar.gz "./imagenes" "./nom_img.txt" "./nom_val.txt" "./nom_a.txt" "../imagenes_reducidas"
-
-
-cat nom_img.txt
-cat nom_val.txt
-cat nom_a.txt
-
+exit 0
